a5e130910989478208c0e079181b82637280dc49
Hackchobo <130716383+Hackchobo@users.noreply.github.com>
2023-05-12 17:47:23 +0900

    05/12

--- 쿼리 #10.sql
+++ 쿼리 #10.sql
@@ -49,7 +49,7 @@ SELECT'customer' AS typ, first_name, last_name
 		FROM actor
 		WHERE first_name REGEXP '^J' AND last_name REGEXP '^D';
 
-
+-- intersect
 SELECT A.first_name, A.last_name
 FROM (
 		SELECT'customer' AS typ, first_name, last_name
@@ -122,7 +122,7 @@ VALUES
 	'This is text data'
 );
 
-SELECT *, QUOTE(text_fld) from string_tbl;
+SELECT *, QUOTE(text_fld) from string_tbl; -- QUOTE 이스케이프 문자까지 찍어주는 명령어
 
 UPDATE string_tbl
 SET vchar_fld = 'This in a piece of extremely long varchar data';
@@ -130,19 +130,26 @@ SET vchar_fld = 'This in a piece of extremely long varchar data';
 UPDATE string_tbl
 SET vchar_fld = 'This string didn\'t WORK, but it does now';
 
-SELECT @@SESSION.sql_mode;
+SELECT @@SESSION.sql_mode; 
+-- @@session -> 나의 개인 공간 (저장공간) 
+-- 크롬을 키면 유일한 ID를 만든다. 요청을 했는데 처음왔으니까 서버 메모리에서 세션을 만들어서 부여  
+-- 5분까지는 세션유지 요청하면 다시 리셋 (예전은 쿠키) PK값으로 서버에서는 구분을 한다. Wep tokken
+-- 구분할수있도록 세션이라는 것을 사용한다. 브라우저에서 완전히 껏다가 다시들어가야지 세션이 날라가서
+-- 다시 로그인을 할수있음 <-> 웹토큰,JSON,JSP (껏다가 다시켜도 로그인정보가 담겨있으면)
+-- pageContext, session, application, request
 
-SET sql_mode='strict';
+SET sql_mode='strict'; -- 일반적으로 strict문자이다 (30개의 문자열을 저장함 그이상은 저장 X) <->
+--  ansi(30개이상의 문자열을 나타내고 다음 문자열은 잘라버린다.)
 
 SHOW WARNINGS;
 
-SELECT lname,fname, CONCAT(lname, ' ',fname)
+SELECT lname,fname, CONCAT(lname, ' ',fname) -- 문자열 합치기
 FROM person;
 
-SELECT lname, CHAR_LENGTH(lname)
+SELECT lname, CHAR_LENGTH(lname) -- 문자열의 길이 LENGTH 보다 CHAR_LENGTH 을 쓰는 이유는 전부다 3byte 로 취급을 한다.
 FROM person;
 
-SELECT lname, POSITION('ur'IN lname)
+SELECT lname, POSITION('ur'IN lname) -- ur의 위치를 숫자로 나타내준다.
 FROM person;
 
 SELECT * FROM string_tbl;
@@ -153,13 +160,13 @@ FROM string_tbl;
 SELECT '안녕' = '안녕'
 		, 'abc' = 'ABC'
 		, 'abc'='cba'
-		, STRCMP('abc','ABCC')
+		, STRCMP('abc','ABCC') -- 같은지 아닌지 비교하는 것
 		, STRCMP('abc','ab');
 		
 SELECT
 	NAME,
 	NAME LIKE '%y',
-	NAME REGEXP '^[C]'
+	NAME REGEXP '^[C]' -- 트루 펄스 판별해주는 것
 FROM category;
 
 SELECT first_name || ' ' || last_name OR 1
@@ -173,7 +180,8 @@ SELECT customer_id,first_name
 FROM customer
 WHERE customer_id = 41;
 
-SELECT 'goobye world', INSERT('goodbye world',9,0,'cruel ');
+SELECT 'goodbye world', INSERT('goodbye world',9,0,'cruel ')
+, INSERT('goodbye asdf sdfsdf ssdfsdf world',POSITION('world' IN 'goodbye asdf sdfsdf ssdfsdf world'),CHAR_LENGTH('world'),'cls');
 
 -- 영화 제목 빈칸에 NICE 추가해주세요.
 
@@ -221,8 +229,8 @@ UPDATE employees
 SET birth_date = DATE_ADD(birth_date, INTERVAL '2-1' YEAR_MONTH)
 WHERE emp_no = 10001;
 
-SELECT CURDATE()
-		, SYSDATE()
+SELECT CURDATE() -- 
+		, SYSDATE() -- 시스템의 연도 월 일 시분초
 		, WEEKDAY(NOW()) -- 월(0),화(1),수(2),목(3),금(4),토(5),일(6) 
 		, DAYNAME(NOW())
 		, LAST_DAY(NOW()) -- 그달의 마지막일
@@ -230,3 +238,10 @@ SELECT CURDATE()
 		, DATE_SUB(NOW(), INTERVAL '2' YEAR)
 		, EXTRACT(MONTH FROM NOW())
 		, DATEDIFF('2023-09-20 00:00:00', NOW());
+		
+-- 7.5.1
+SELECT 'Please find the substring in this string', SUBSTR('Please find the substring in this string', 17, 9);
+-- 7.5.2
+SELECT ABS(-25.76823),sign(-25.76823),ROUND (-25.76823,2); -- 반올림 필요없을때는 truncate
+-- 7.5.3
+SELECT EXTRACT(MONTH FROM NOW()), Month(NOW());
---
+++ 쿼리 #11.sql
@@ -0,0 +1,200 @@
+-- 05/12
+SELECT customer_id, COUNT(1) FROM rental
+GROUP BY customer_id
+ORDER BY COUNT(1) DESC;
+
+-- limit(1), limist(1,1) 
+
+SELECT * FROM rental
+ORDER BY rental_id desc
+LIMIT 2, 3;
+
+-- join 이용하여 사용자 이름 찍어주세요.
+SELECT  B.first_name, B.last_name, A.customer_id, COUNT(1) cnt
+FROM rental A
+INNER JOIN customer B
+on A.customer_id = B.customer_id
+GROUP BY A.customer_id
+ORDER BY COUNT(1) DESC
+LIMIT 1;
+
+SELECT B.customer_id, B.first_name, B.last_name, A.cnt
+from
+(
+	select customer_id, COUNT(1) cnt
+	FROM rental 
+	GROUP BY customer_id
+	ORDER BY COUNT(1) DESC
+	LIMIT 1
+) A
+INNER JOIN customer B
+ON A.customer_id = B.customer_id;
+
+-- 가장 적게 빌린 사람의 pk, 이름, 빌린 수
+SELECT B.customer_id, B.first_name, B.last_name, A.cnt
+FROM(
+	select customer_id, COUNT(1) cnt
+	FROM rental 
+	GROUP BY customer_id
+	ORDER BY COUNT(1)
+	LIMIT 1
+)A
+INNER JOIN customer B
+ON A.customer_id = B.customer_id;
+
+SELECT customer_id, COUNT(1) vovo
+FROM rental
+GROUP BY customer_id
+ORDER BY 2 DESC;
+
+-- 렌탈횟수가 40회이상인 사람들 pk, 이름, 성, 렌탈수
+SELECT B.customer_id,B.first_name,B.last_name, COUNT(1) cnt
+FROM rental A
+JOIN customer B
+ON A.customer_id = B.customer_id
+GROUP BY B.customer_id
+HAVING COUNT(1) >= 40;
+
+SELECT B.customer_id,B.first_name,B.last_name, A.cnt
+FROM(
+	SELECT customer_id,COUNT(1) cnt
+	FROM rental
+	GROUP BY customer_id
+	HAVING cnt >=40
+) A
+INNER JOIN customer B
+ON A.customer_id = B.customer_id
+ORDER BY A.cnt;
+
+-- max, min, avg, count, sum
+
+SELECT customer_id
+, MAX(amount), MIN(amount)
+, AVG(amount), SUM(amount) / COUNT(amount) 
+FROM payment
+GROUP BY customer_id;
+
+SELECT COUNT(customer_id)
+, COUNT(DISTINCT customer_id)
+FROM payment;
+
+SELECT COUNT(1)
+FROM customer;
+
+SELECT *
+FROM rental
+WHERE return_date IS NULL;
+
+SELECT COUNT(*), COUNT(return_date), COUNT(1)
+FROM rental
+WHERE return_date IS NULL;
+
+SELECT MAX(DATEDIFF(return_date, rental_date))
+FROM rental;
+
+-- 제일 늦게 반납한 사람 정보, pk, 이름, 성 
+SELECT B.customer_id, B.first_name, B.last_name, max(DATEDIFF(return_date, rental_date))
+FROM rental A
+inner JOIN customer B
+ON A.customer_id = B.customer_id;
+
+SELECT B.last_name, B.first_name, B.customer_id
+FROM(
+	SELECT DISTINCT customer_id
+	FROM rental
+	WHERE DATEDIFF(return_date, rental_date)=
+		(
+			SELECT MAX(DATEDIFF(return_date, rental_date))
+			FROM rental
+		)
+)A
+INNER JOIN customer B
+ON A.customer_id = B.customer_id
+ORDER BY B.customer_id;
+
+-- 배우의 등급별 출연 횟수
+SELECT A.actor_id, B.rating, COUNT(1) cnt
+FROM film_actor A
+JOIN film B
+ON A.film_id = B.film_id
+GROUP BY A.actor_id, B.rating -- 그룹 바이를 다중 그룹으로 하면 기준이 똑같아야 된다.
+ORDER BY cnt DESC; -- 이것까지 하면 한영화 배우가 어디 등급에 가장많이 출연했는지 알수있게 된다.
+
+-- 배우의 카테고리별 출연횟수
+SELECT A.category_id, COUNT(1) cnt
+FROM category A
+JOIN film_category B
+ON A.category_id= B.category_id
+GROUP BY A.category_id;
+
+SELECT A.actor_id,D.first_name,D.last_name, B.category_id,C.name, COUNT(1) cnt
+FROM film_actor A
+JOIN film_category B
+ON A.film_id = B.film_id
+JOIN category C
+ON B.category_id = C.category_id
+JOIN actor D
+ON A.actor_id = D.actor_id
+GROUP BY A.actor_id, B.category_id;
+
+-- 서브 쿼리 이용
+SELECT C.actor_id,D.first_name,D.last_name, C.category_id, COUNT(1) cnt
+FROM (SELECT A.actor_id,B.category_id, COUNT(1) cnt
+		FROM film_actor A
+		JOIN film_category B
+		ON A.film_id = B.film_id
+		GROUP BY A.actor_id,B.category_id
+)C
+JOIN actor D
+ON C.actor_id = D.actor_id
+GROUP BY C.actor_id, C.category_id;
+
+-- 정호씨 코드
+SELECT A.actor_id,B.category_id, COUNT(1) cnt
+FROM film_actor A
+JOIN film_category B
+ON A.film_id = B.film_id
+GROUP BY A.actor_id,B.category_id;
+
+-- 연도별 렌탈 횟수 궁금쓰...
+SELECT  CONCAT(year(rental_date),'년') AS `연도`,count(1) `갯수`
+FROM rental 
+GROUP BY `연도`;
+
+-- 롤업생성
+SELECT fa.actor_id, f.rating, COUNT(1) AS cnt
+FROM film_actor fa
+INNER JOIN film f
+ON fa.film_id = f.film_id
+GROUP BY fa.actor_id,f.rating WITH ROLLUP; -- actor_id의 PK값의 rating에는 null값이 들어간다.
+-- null값의 의미는 PK값의 전체 합계이다.
+
+-- 배우의 등급('G', 'PG') 별 출연 횟수가 궁금함, 출연횟수가 9초과인 actor_id 궁금쓰
+SELECT A.actor_id, B.rating, COUNT(1) cnt
+FROM film_actor A
+JOIN film B
+ON A.film_id = B.film_id
+WHERE B.rating IN('G','PG') 
+GROUP BY A.actor_id, B.rating 
+HAVING COUNT(1)>9
+ORDER BY cnt;
+-- 선생님 코드
+SELECT A.actor_id, B.rating, COUNT(1) cnt
+FROM film_actor A
+JOIN film B
+ON A.film_id = B.film_id
+GROUP BY A.actor_id, B.rating 
+HAVING B.rating IN('G','PG') and COUNT(1)>9
+ORDER BY cnt;
+
+-- 8.5.1
+SELECT COUNT(1) FROM payment;
+-- 8.5.2
+SELECT customer_id, COUNT(1) cnt, SUM(amount)
+FROM payment
+GROUP BY customer_id WITH rollup;
+-- 8.5.3
+SELECT customer_id, COUNT(1) cnt, SUM(amount)
+FROM payment
+GROUP BY customer_id
+HAVING cnt>=40;
